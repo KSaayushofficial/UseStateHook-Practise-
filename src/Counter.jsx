a480317d06e8ff722e72fa1578f2acd768530c0d
KSaayushofficial <ksaayushofficial3@gmail.com>
2025-02-04 17:43:09 +0545

    Counter app using usestate

---
+++ src/Counter.jsx
@@ -0,0 +1,23 @@
+import { useState } from 'react'
+
+export default function Counter() {
+    const [count,setCount] = useState(0);
+
+    function increment(){
+        setCount(count+1);
+    }
+    function decrement() {
+      setCount(count - 1);
+    }
+    function reset() {
+      setCount(0);
+    }
+  return (
+    <>
+      <h1>{count}</h1>
+      <button onClick={increment}>Increment</button>
+      <button onClick={reset}>Reset</button>
+      <button onClick={decrement}>Decrement</button>
+    </>
+  );
+}
